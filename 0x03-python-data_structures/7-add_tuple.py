37ba6c35749f4deb58c2162586c3003abca7a928
ezakariaa <zakariaorche@gmail.com>
2023-12-04 13:56:18 -0800

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
+    mytuple = ()
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    mytuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return mytuple

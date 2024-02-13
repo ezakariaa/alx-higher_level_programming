79a9106ae51383a59d4b7c979c623fc170c28998
ezakariaa <zakariaorche@gmail.com>
2024-02-13 12:01:32 -0800

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Defines two integer addition function."""
+
+
+def add_integer(a, b=98):
+    """adds two integers & returns the addition.
+
+    args:
+    a (int or float): First number to be added
+    b (int or float): Second number to be added
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")

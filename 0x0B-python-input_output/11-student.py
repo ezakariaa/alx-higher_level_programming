81b6bef4c21f8dda9faa779a353657a0d898cc36
ezakariaa <zakariaorche@gmail.com>
2024-02-20 16:32:03 -0800

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """Represents Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the attributes
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): The attributes.
+        """
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ that replaces all attributes of the Student instance.
+
+        Args:
+            json (dict): A dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)

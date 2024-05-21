480db3c4cd22c4f4fd5da7ec17986c1ac78488a4
ezakariaa <zakariaorche@gmail.com>
2024-05-21 15:48:39 -0700

    10. List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+--  a script that lists all records of the table second_table of the database
+-- Records should be ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;

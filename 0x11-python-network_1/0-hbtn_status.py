9197dd0521dd31015a56de8c131ca0d153a3bf6a
ezakariaa <zakariaorche@gmail.com>
2024-07-05 14:28:34 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 
 import urllib.request
 
+
 url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
@@ -12,4 +13,3 @@ print("Body response:")
 print(f"\t- type: {type(body)}")
 print(f"\t- content: {body}")
 print(f"\t- utf8 content: {body.decode('utf-8')}")
-

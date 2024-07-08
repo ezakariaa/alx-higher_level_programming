3b83a9f93f80452517f5e0946825795ffb464f73
ezakariaa <zakariaorche@gmail.com>
2024-07-07 17:15:12 -0700

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request to the passed URL 
+script that takes in a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of the response (decoded in utf-8)
 """
 
@@ -13,16 +13,16 @@ if len(sys.argv) < 3:
     print("Usage: python script_name.py <URL> <email>")
     sys.exit(1)
 
-    url = sys.argv[1]
-    email = sys.argv[2]
+url = sys.argv[1]
+email = sys.argv[2]
 
-    # Encode the email data
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('utf-8')  # Convert to bytes
+# Encode the email data
+data = urllib.parse.urlencode({'email': email})
+data = data.encode('utf-8')  # Convert to bytes
 
-    req = urllib.request.Request(url, data=data, method='POST')
+req = urllib.request.Request(url, data=data, method='POST')
 
-    with urllib.request.urlopen(req) as response:
-        # Decoding and displaying the body of the response
-        body = response.read().decode('utf-8')
-        print(body)
+with urllib.request.urlopen(req) as response:
+    # Decoding and displaying the body of the response
+    body = response.read().decode('utf-8')
+    print(body)

68652fe9cb00a2370e2680e88e475a77811b37f8
ezakariaa <zakariaorche@gmail.com>
2024-07-09 15:22:27 -0700

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,14 +5,9 @@ The email must be sent in the variable email
 You must use the packages requests and sys
 You are not allowed to import packages other than requests and sys
 You don’t need to error check arguments passed to the script (number or type)"""
-
-import sys
+from sys import argv
 import requests
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
-    print(r.text)
+if __name__ == '__main__':
+    print(requests.post(argv[1], data={'email': argv[2]}).text)

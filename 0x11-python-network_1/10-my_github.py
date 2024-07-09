7c95fa41689dce202070f1bc49e2398ab0888ad9
ezakariaa <zakariaorche@gmail.com>
2024-07-09 15:42:08 -0700

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 #uses the GitHub API to display your id
-
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print(user_info.get("id"))
+    else:
+        print("Not a valid JSON")

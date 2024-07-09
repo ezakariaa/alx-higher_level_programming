1dcacf32783d8ccda52bf8ef52b987bb33e3afd2
ezakariaa <zakariaorche@gmail.com>
2024-07-09 15:12:40 -0700

    5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,14 +4,7 @@
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-
-    x_request_id = response.headers.get('X-Request-Id')
-
-    if x_request_id is not None:
-        print(x_request_id)
-    else:
-        print("X-Request-Id not found in the response headers.")
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))

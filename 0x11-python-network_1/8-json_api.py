0bd776a5dd071d08793d217167421a03bacd27a5
ezakariaa <zakariaorche@gmail.com>
2024-07-09 15:37:47 -0700

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 #script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-
-import requests
 import sys
-
-
+import requests
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        q = ""
-    else:
-        q = sys.argv[1]
-
-        url = 'http://0.0.0.0:5000/search_user'
-        data = {'q': q}
-
-        response = requests.post(url, data=data)
-        try:
-            json_response = response.json()
-            if json_response:
-                print(f"[{json_response['id']}] {json_response['name']}")
-            else:
-                print("No result")
-        except ValueError:
-            print("Not a valid JSON")
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
